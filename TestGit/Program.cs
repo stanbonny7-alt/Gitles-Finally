437c8bbeb06b127a298cb2d2ddf9292faedcb563
ijzer-beest <stan.bonny.7@gmail.com>
2026-02-10 11:57:06 +0100

    Update message about eating jagersworst

--- TestGit/Program.cs
+++ TestGit/Program.cs
@@ -6,7 +6,7 @@
         {
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Nico Devriendt!");
-            Console.WriteLine("Ik eet graag jagersworst");
+            Console.WriteLine("Ik eet graag jagersworst met frietjes");
         }
     }
 }
